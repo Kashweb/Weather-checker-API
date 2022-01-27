9d215e6cabb688fcef8337cc03a5f53f95701495
Kashish Chawla <69769135+kashishchawla1@users.noreply.github.com>
2022-01-27 21:22:15 +0930

    Lets get the JS now

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-let weather ={
+const weather ={
     "api" = "74ee123ab6f6950bfd34ae4827a70492",
     getweather: function() {
         fetch()
     }
 
 }
+
+https://api.openweathermap.org/data/2.5/weather?q=Darwin&appid=74ee123ab6f6950bfd34ae4827a70492
+
+
+https://api.openweathermap.org/data/2.5/weather?q=Darwin&units=metric&appid=74ee123ab6f6950bfd34ae4827a70492
+
+https://api.openweathermap.org/data/2.5/onecall?lat=-12.462827&lon=130.841782&exclude=current,minutely,hourly,alerts&appid=74ee123ab6f6950bfd34ae4827a70492
