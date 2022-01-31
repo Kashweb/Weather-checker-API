950fe31ad7db8f999ee9870e5935d9175b971672
Kashish Chawla <69769135+kashishchawla1@users.noreply.github.com>
2022-01-31 20:08:02 +0930

    Fetch using API

--- script.js
+++ script.js
@@ -1,24 +1,16 @@
-let weather ={
-    "api" = "74ee123ab6f6950bfd34ae4827a70492",
-    fetchweather: function(location) {
-        fetch("https://api.openweathermap.org/data/2.5/weather?q="
-        + city
-        + "&units=metric&appid="\
-        +this.api
-        .then(responce => {
-            return responce,json();
-        }) .then(function(responce) {
-            console.log(responce.coord);
+let weather = {
+  apiKey: "74ee123ab6f6950bfd34ae4827a70492",
+  fetchWeather: function (city) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" 
+      + city 
+      + "&units=metric&appid=" 
+      + this.apiKey
+    )
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+    },
+    displayWeather: function(data){
 
-        })
-
-        
-
-
-
-https://api.openweathermap.org/data/2.5/weather?q=Darwin&appid=74ee123ab6f6950bfd34ae4827a70492
-
-
-https://api.openweathermap.org/data/2.5/weather?q=Darwin&units=metric&appid=74ee123ab6f6950bfd34ae4827a70492
-
-https://api.openweathermap.org/data/2.5/onecall?lat=-12.462827&lon=130.841782&exclude=current,minutely,hourly,alerts&appid=74ee123ab6f6950bfd34ae4827a70492
+    }
+  };
