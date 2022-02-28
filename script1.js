7e5bc64fa824469eecd3ed15f5fc4dace4dc3015
Kashish Chawla <69769135+kashishchawla1@users.noreply.github.com>
2022-02-28 20:19:42 +0930

    Update script1.js
    geolocation

--- script1.js
+++ script1.js
@@ -15,8 +15,16 @@ searchbar.addEventListener("keyup", e => {
     }
 });
 
+getLocation.addEventListener("click", () => {
+    if(!navigator.geolocation) {
+        alert("The Browswer does not support Geolocation...")
+    }
+    else{
+        navigator.geolocation.getCurrentPosition(getGeoLocation, error);
+    }
+})
 
-    
+
 function fetchWeather(city){
     fetch("https://api.openweathermap.org/data/2.5/weather?q=city&units=metric&appid=74ee123ab6f6950bfd34ae4827a70492")
     .then((response) => response.json())
@@ -27,7 +35,14 @@ function getGeoLocation(position){
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
     fetch("https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=74ee123ab6f6950bfd34ae4827a70492")
-}
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+};
+
+function error(error){
+    console.log("hello")
+};
+
 
 
     
