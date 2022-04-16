94be638ed881845d866246656dc780f3afd29f87
Kashish Chawla <69769135+kashishchawla1@users.noreply.github.com>
2022-04-16 19:27:10 +0930

    quiz app js

--- JavaScript/quizscript.js
+++ JavaScript/quizscript.js
@@ -1,11 +1,11 @@
 const startButton = document.querySelector(".startbutton");
 activeInfo = document.querySelector(".activeinfo");
+quizBox = document.querySelector(".quizbox");
 
 startButton.addEventListener("click", e =>{
     hel()
 });
 
 function hel(){
-    activeInfo.classList.remove("activeInfo")
-    alert("hello")
-}
+    quizBox.classList.add("active");
+};
