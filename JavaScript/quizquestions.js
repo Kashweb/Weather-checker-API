93cb123b903bc4d7950bb0c3c85dbfd202854d3e
Kashish Chawla <69769135+kashishchawla1@users.noreply.github.com>
2022-04-17 19:27:08 +0930

    quiz questions

---
+++ JavaScript/quizquestions.js
@@ -0,0 +1,59 @@
+// creating an array and passing the number, questions, options, and answers
+
+let questions = [
+    {
+    numb: 1,
+    question: "In which year was the first weather satellite launched?",
+    answer: "1959",
+    options: [
+      "1970",
+      "1959",
+      "1934",
+      "1992"
+    ]
+  },
+    {
+    numb: 2,
+    question: "The increasing concentration of which gas is a main cause of global warming?",
+    answer: "Carbon dioxide",
+    options: [
+      "Carbon dioxide",
+      "Sulphur dioxide",
+      "Oxygen",
+      "Nitrous oxide"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which is the technical term for the moisture content in the air?",
+    answer: "Humility",
+    options: [
+      "Condensation",
+      "Humility",
+      "Humility",
+      "Precipitation"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What is the layer of air that surrounds the Earth called?",
+    answer: "Atmosphere",
+    options: [
+      "Water Vapor",
+      "Atmosphere",
+      "Mantle",
+      "Ozone layer"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What is the name of instrument used to measure Temprature?",
+    answer: "Thermometer",
+    options: [
+      "Hygrometer",
+      "Thermometer",
+      "eXTra Multi-Program Language",
+      "Hydrometer"
+    ]
+  },
+];
--- JavaScript/quizscript.js
+++ JavaScript/quizscript.js
@@ -1,11 +1,22 @@
+
+// selecting all the HTML elements. 
 const startButton = document.querySelector(".startbutton");
 activeInfo = document.querySelector(".activeinfo");
 quizBox = document.querySelector(".quizbox");
+timeSeconds = document.querySelector(".timeseconds");
+Question = document.querySelector(".question span");
+Options1 = document.querySelector(".option");
+Options2 = document.querySelector(".option");
+
+
 
 startButton.addEventListener("click", e =>{
-    hel()
+    quizBox.classList.add("active");
+    showQuestions();
 });
 
-function hel(){
-    quizBox.classList.add("active");
-};
+function showQuestions() {
+    Question.innerHTML = questions[0].question;
+}
+
+
