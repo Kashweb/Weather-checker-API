bf084bc23af9949d22db66250967cbe74cfe7e74
Kashish Chawla <69769135+kashishchawla1@users.noreply.github.com>
2022-04-25 22:12:00 +0930

    quiz app DOM

--- JavaScript/quizscript.js
+++ JavaScript/quizscript.js
@@ -2,9 +2,11 @@
 // selecting all the HTML elements. 
 let currentQuestion = 0;
 let currentScore = 0;
+let score = 0;
 
 
 const startButton = document.querySelector(".startbutton");
+startSection = document.querySelector(".startsection");
 activeInfo = document.querySelector(".activeinfo");
 quizBox = document.querySelector(".quizbox");
 timeSeconds = document.querySelector(".timeseconds");
@@ -15,34 +17,48 @@ Options = document.querySelector(".options");
 submitButton = document.querySelector(".submitbutton");
 prevButton = document.querySelector(".buttonprev");
 nextButton = document.querySelector(".buttonnext");
-resultsBox = document.querySelector(".results");
+resultsBox = document.querySelector(".resultshide");
 restartButton = document.querySelector(".results button");
-
+score = document.querySelector(".score span");
 
 
 startButton.addEventListener("click", e =>{
+    startSection.classList.add("actives");
     quizBox.classList.add("active");
     showQuestions(currentQuestion);
+    
 });
 
 
 function showQuestions(currentQuestion) {
     Question.innerHTML = questions[currentQuestion].numb + ": " + " " +questions[currentQuestion].question;
 
-let OptionList = '<button class="option">'+ questions[currentQuestion].options[0] +'</button>'
+let options = '<button class="option">'+ questions[currentQuestion].options[0] +'</button>'
                 + '<button class="option">'+ questions[currentQuestion].options[1] +'</button>'
                 + '<button class="option">'+ questions[currentQuestion].options[2] +'</button>'
                 + '<button class="option">'+ questions[currentQuestion].options[3] +'</button>'
             
-    Options.innerHTML = OptionList;
+    Options.innerHTML = options;
     nextbutton();
     prevbutton();
-}
+};
+
+function correctAnswer(currentQuestion){
+
+};
+
+
 
 submitButton.addEventListener("click", e =>{
-    showQuestions(currentQuestion++);
-    nextbutton();
-    showQuestions(currentQuestion);
+    if (currentQuestion < questions.length - 1){
+        showQuestions(currentQuestion++);
+        nextbutton();
+        showQuestions(currentQuestion);
+    }else {
+        startSection.classList.add("actives");
+        quizBox.classList.remove("active");
+        resultsBox.classList.add("results");
+    } 
 });
 
 nextButton.addEventListener("click", e =>{
@@ -73,8 +89,4 @@ restartButton.addEventListener("click", e =>{
     quizBox.classList.add("active");
     showQuestions(currentQuestion);
     resultsBox.classList.add("resultshide");
-
 });
-
-
-
